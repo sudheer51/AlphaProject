8c8abd4d220af400ea49c034da31fc17df10f04f
aqib0098 <saqib5029@gmail.com>
2018-01-15 18:08:36 -0600

    test

---
+++ test/src/test/jeken2.java
@@ -0,0 +1,61 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
+
+public class jeken2{
+	
+	static WebDriver driver;
+	@BeforeClass
+	public static void invoke()
+	{
+	System.setProperty("webdriver.gecko.driver", "\\C:\\Users\\Aqib\\Desktop\\geckodriver.exe\\");
+	 driver =new FirefoxDriver(); 
+	 //System.setProperty("webdriver.gecko.driver", "\\C:\\Users\\Aqib\\Desktop\\geckodriver.exe\\");	
+	driver.get("https://www.facebook.com/");
+	
+	}
+	@AfterClass 
+	public static void Quite() throws InterruptedException
+	{
+		//Thread.sleep(5000);
+		driver.quit();
+	}
+	
+	
+	
+	
+	
+	
+	
+	@Test (priority=3)
+	public void tes009() throws InterruptedException
+	{
+	  
+		//System.setProperty("webdriver.gecko.driver", "\\C:\\Users\\Aqib\\Desktop\\geckodriver.exe\\");
+		
+		 //WebDriver driver = new FirefoxDriver();
+	  
+	 // driver.get("https://www.facebook.com/");
+	 
+              //Thread.sleep(1000);	  
+		driver.findElement(By.id("email")).sendKeys("ajkdkdnakjn");
+		System.out.println("this is third");
+	  }
+	@Test (priority =4)
+	public void test0011()
+	{
+		
+		driver.findElement(By.id("pass")).sendKeys("weqeewr");
+		
+		
+		System.out.println("this is fourth");
+	}
+	
+	
+}
